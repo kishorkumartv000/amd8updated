dd6dd640219d900d360af8ce81e3ef58c7afe092
Neon Light <kishorkumartv000@gmail.com>
2025-08-10 04:18:51 +0530

    Update __init__.py

--- bot/providers/apple/__init__.py
+++ bot/providers/apple/__init__.py
@@ -1,1 +1,10 @@
-from .apple import start_apple
+from .apple import AppleMusicCore, start_apple
+from .downloader import run_apple_downloader
+from .metadata import extract_apple_metadata
+
+__all__ = [
+    'AppleMusicCore',
+    'start_apple',
+    'run_apple_downloader',
+    'extract_apple_metadata'
+]
